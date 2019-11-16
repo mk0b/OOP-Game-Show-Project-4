175494a3e591beac481acab2e6dfd6ee2858bb50
Megan Katherine O'Brien <megankob@gmail.com>
2019-11-15 22:58:50 -0500

    startGameButton Working
    Cleaned up some code in app.js and got the startGameButton event listener working.

--- js/app.js
+++ js/app.js
@@ -10,20 +10,35 @@
 //TODO: After they guess it correctly, show the meaning of the phrase/idiom. Or a hint before hand?
 //TODO: Document my methods correctly. Look up again how to do that.
 
-const game = new Game();
+
 
 //TODO: Remove. Testing phrases array.
-game.phrases.forEach((phrase, index) => {
-    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-});
+//game.phrases.forEach((phrase, index) => {
+    //console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+//});
 
 //testing the getRandomPhrase()
-console.log(game.getRandomPhrase());
+//console.log(game.getRandomPhrase());
 
 
 //testing addPhraseToDisplay()
 //game.getRandomPhrase().addPhraseToDisplay();
 
 //Testing game.startGame();
-game.startGame();
-console.log('Active Phrase - phrase: ' , game.activePhrase.phrase);
+//game.startGame();
+//console.log('Active Phrase - phrase: ' , game.activePhrase.phrase);
+
+/*
+Real code below this line | Bulk Test Code to be removed above.
+*/
+
+//Declarations
+let game;
+const startGameButton = document.querySelector('#btn__reset');
+
+//Event Listeners
+
+startGameButton.addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
